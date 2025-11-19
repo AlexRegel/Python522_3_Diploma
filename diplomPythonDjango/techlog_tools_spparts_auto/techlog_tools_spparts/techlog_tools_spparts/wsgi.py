1007593c941b18bfed751001440867f8bb26fb4d
AlexRegel <alexanfedich888@mail.ru>
2025-11-19 17:47:00 +0300

    first commit diploma PyDjango

---
+++ diplomPythonDjango/techlog_tools_spparts_auto/techlog_tools_spparts/techlog_tools_spparts/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for techlog_tools_spparts project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techlog_tools_spparts.settings')
+
+application = get_wsgi_application()
