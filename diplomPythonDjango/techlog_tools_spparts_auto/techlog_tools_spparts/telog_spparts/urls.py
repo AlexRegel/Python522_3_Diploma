0b83cc3759c2f6d00817570f916590345df6440a
AlexRegel <alexanfedich888@mail.ru>
2025-12-31 21:42:10 +0300

    Task_11_forDZ_88_Py17_roller separation and editing repairs

--- diplomPythonDjango/techlog_tools_spparts_auto/techlog_tools_spparts/telog_spparts/urls.py
+++ diplomPythonDjango/techlog_tools_spparts_auto/techlog_tools_spparts/telog_spparts/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.telog_spparts, name='telogspparts'),
     path('<int:telog_spparts_id>', views.sppart_details, name='sppart_details'),
-    path('repairs/', views.create_repairs, name='createautorepairs'),
+    # path('new_repairs/', views.create_repairs, name='createrepairs'),
+    path('current_new_repairs/', views.current_new_repairs, name='currentnewrepairs'),
+    path('current_new_repairs/<int:repair_pk>', views.view_repair, name='viewrepair'),
 ]
-# в base.html используется, например name='createautorepairs'
-# в теге  <a href="{% url 'createautorepairs' %}"> %}", как
+# в base.html используется, например name='currentnewrepairs'
+# в теге  <a href="{% url 'currentnewrepairs' %}"> %}", как
 # ссылка на модуль создания / проведения новых ремонтов
--- diplomPythonDjango/techlog_tools_spparts_auto/techlog_tools_spparts/telog_spparts/views.py
+++ diplomPythonDjango/techlog_tools_spparts_auto/techlog_tools_spparts/telog_spparts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import TelogSpparts, Repairs
 from techlog_tools.forms import RepairsForm
 
@@ -17,9 +17,46 @@ def sppart_details(request, telog_spparts_id):
     return render(request, 'telog_spparts/sppart_details.html', {'sppart': sppart})
 
 
-#  Функция создания/обработки ремонтов
-def create_repairs(request):
-    # repairs = Repairs.objects.all()
+#  Функция текущих/новых авторемонтов (ремонтов)
+def current_new_repairs(request):
+    # __isnull - постфикс для поля завершения 'ready' ремонтов
+    repairs = Repairs.objects.filter(user_performer=request.user, ready__isnull=True)
     if request.method == 'GET':
-        return render(request, 'telog_spparts/repairs.html', {'form': RepairsForm()})
-    # {'repairs': repairs}
+        return render(request, 'telog_spparts/repairs.html', {'repairs': repairs, 'form_rep': RepairsForm()})
+    else:
+        try:
+            form = RepairsForm(request.POST)
+            # создаём новую переменную из своей формы (forms.py)
+            new_repairs = form.save(commit=False)  # commit=False - не сохраняя в БД
+            # Присвоение значения текущего пользователя.
+            new_repairs.user_performer = request.user  # Важно!!! - именно user, а не user_performer
+            new_repairs.save()
+            return redirect('currentnewrepairs')
+        except ValueError:
+            context = {'repairs': repairs, 'form_rep': RepairsForm(),
+                       'error': 'Переданы неверные данные. Попробуйте повторно.'}
+            return render(request, 'telog_spparts/repairs.html',
+                          context)
+
+
+# Функция текущих ремонтов
+# (объеденена с предыдущей, незадействована)
+# def current_repairs(request):
+#     # __isnull - постфикс для поля завершения 'ready' ремонтов
+#     repairs = Repairs.objects.filter(user_performer=request.user, ready__isnull=True)
+#     return render(request, 'telog_spparts/repairs.html', {'repairs': repairs, 'form_rep': RepairsForm()})
+
+
+def view_repair(request, repair_pk):
+    repair = get_object_or_404(Repairs, pk=repair_pk)
+    if request.method == "GET":
+        repform = RepairsForm(instance=repair)
+        return render(request, 'telog_spparts/viewrepair.html', {'repair': repair, 'repform': repform})
+    else:
+        try:
+            repform = RepairsForm(request.POST, instance=repair)
+            repform.save()
+            return redirect('currentnewrepairs')
+        except ValueError:
+            return render(request, 'telog_spparts/viewrepair.html',
+                          {'repair': repair, 'repform': repform, 'error': "Неверные данные"})
